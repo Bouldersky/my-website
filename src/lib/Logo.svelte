439298253bcc266af9e41b1ebe0436345e39df77
bouldersky <skyarnold1@me.com>
2022-11-10 18:57:09 -0700

    feat: navbar with logo

---
+++ src/lib/Logo.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	export let clazz: string;
+	export { clazz as class };
+</script>
+
+<svg class={clazz} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1273.92 2206.54">
+	<g id="Layer_1" data-name="Layer 1">
+		<polygon class="cls-1" points="0 1838.8 636.96 1471.05 636.96 2206.54 0 1838.8" />
+	</g>
+	<g id="Layer_2" data-name="Layer 2">
+		<polygon class="cls-5" points="636.96 2206.54 636.96 1471.05 1273.92 1838.8 636.96 2206.54" />
+	</g>
+	<g id="Layer_3" data-name="Layer 3">
+		<polygon class="cls-6" points="1273.92 1838.8 636.96 1471.05 1273.92 1103.3 1273.92 1838.8" />
+	</g>
+	<g id="Layer_4" data-name="Layer 4">
+		<polygon class="cls-2" points="1273.92 1103.24 636.96 1470.99 636.96 735.5 1273.92 1103.24" />
+	</g>
+	<g id="Layer_5" data-name="Layer 5">
+		<polygon class="cls-4" points="636.96 735.5 636.96 1470.99 0 1103.24 636.96 735.5" />
+	</g>
+	<g id="Layer_6" data-name="Layer 6">
+		<polygon class="cls-1" points="636.96 735.5 0 1103.24 0 367.75 636.96 735.5" />
+	</g>
+	<g id="Layer_7" data-name="Layer 7">
+		<polygon class="cls-3" points="636.96 0 636.96 735.5 0 367.75 636.96 0" />
+	</g>
+	<g id="Layer_8" data-name="Layer 8">
+		<polygon class="cls-6" points="636.96 0 636.96 735.5 1273.92 367.75 636.96 0" />
+	</g>
+</svg>
+
+<style>
+	.cls-1 {
+		fill: #1f8fcc;
+	}
+
+	.cls-2 {
+		fill: #135a80;
+	}
+
+	.cls-3 {
+		fill: #27b5ff;
+	}
+
+	.cls-4 {
+		fill: #396780;
+	}
+
+	.cls-5 {
+		fill: #33a6e8;
+	}
+
+	.cls-6 {
+		fill: #73ceff;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	import '../app.css';
+	import Logo from '$lib/Logo.svelte';
 </script>
+
+<nav class="bg-gray-400 p-3 flex justify-between items-center">
+	<Logo class="h-12" />
+</nav>
 
 <slot />
